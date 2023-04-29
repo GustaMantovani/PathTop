2b314d4e53ca0eb612f460e28cebf7b586af334f
GustaMantovani <mantovanig000@gmail.com>
2023-04-29 09:55:16 -0300

    ajustes de usabilidade

--- pathtop.py
+++ pathtop.py
@@ -5,15 +5,7 @@ import subprocess #importa o módulo que permite a execução de comandos no tem
 def list_dirs(): #listar os diretórios em /proc
     #array que armazena os diretórios de /proc
     sysDirs = [f for f in os.listdir('/proc') if f.isdigit()] #função da biblioteca os que lista o diretorio do caminho que recebe em seu parâmetro
-
-    dirs = []
-
-    for pid in sysDirs:
-        intPid = int(pid)
-        if intPid > 1000: #Os PIDs de 1000 à baixo são relacionados ao sistem, as aplicações só se encontram acima disso
-            dirs.append(pid)
-
-    return dirs
+    return sysDirs
 
 def proc_stats(pid):
     process = psutil.Process(int(pid))
@@ -33,13 +25,27 @@ def proc_stats(pid):
 def path_search(path): #procura os processos iniciados pelo binário em questão e exibe as métricas de cada um
     dirs = list_dirs() #obtém o array com os processos de interesse
 
-    for pid in dirs: #estrutura para percorrer todo o array
-        proc_path = f"/proc/{pid}/exe" #caminho para o arquivo que contém o caminho do binário que gerou o processo
-        if os.path.exists(proc_path) and os.path.islink(proc_path): #se o caminho existir e o arquivo for um link simbólico
-            bin_path = os.readlink(proc_path) #usa readlink para obter o caminho do binário a partir do link simbólico utilizando o comando 'readlink' do linux
-            if bin_path == path: #compara a entrada do usuário com o caminho obtido
-                proc_stats(pid) #chama a função que exibe as métricas do processo
+    i = 0
 
-path = str(input("Caminho do binário que gerou seu processo: "))
-path_search(path)
+    for pid in dirs: #estrutura para percorrer todo o array
+        try:
+            proc_path = f"/proc/{pid}/exe" #caminho para o arquivo que contém o caminho do binário que gerou o processo
+        except Exception as e:
+            print(e)
+        else:
+            if os.path.exists(proc_path) and os.path.islink(proc_path): #se o caminho existir e o arquivo for um link simbólico
+                bin_path = os.readlink(proc_path) #usa readlink para obter o caminho do binário a partir do link simbólico utilizando o comando 'readlink' do linux
+
+                if bin_path == path: #compara a entrada do usuário com o caminho obtido
+                    proc_stats(pid) #chama a função que exibe as métricas do processo
+                    i+=1
+    if i == 0:
+        print('Nenhum processo foi gerado pelo executável informado')        
+
+path = None        
+
+while path != 'q':
+    path = str(input("Caminho do binário que gerou seu processo ('q' to quit): "))
+    if path != 'q':
+        info = path_search(path)
 
